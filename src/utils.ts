ecc38a392787705b826ba065de427895da82f9d3
little-buddy <litoubuddy@gmail.com>
2022-07-20 19:08:28 +0800

    refactor: x

--- src/utils.ts
+++ src/utils.ts
@@ -7,17 +7,19 @@ export const formatAddress = (address: string, bit = 8) => {
 
 export const copyTx = (text: string) => copy(text);
 
-export const localeNumber = (num: number, bit = 0, zero = true) => {
+export const localeNumber = (xnum: number, bit = 0, zero = true) => {
+  const num = Math.abs(xnum);
   const intNum = Math.floor(num);
   const floatNum = num - intNum;
 
   return (
-    intNum.toLocaleString() +
-    (bit == 0
-      ? ""
-      : zero
-      ? floatNum.toFixed(bit).substring(1)
-      : new BigNumber(+floatNum.toFixed(bit)).toFixed().substring(1))
+    (xnum > 0 ? "" : "-") +
+    (intNum.toLocaleString() +
+      (bit == 0
+        ? ""
+        : zero
+        ? floatNum.toFixed(bit).substring(1)
+        : new BigNumber(+floatNum.toFixed(bit)).toFixed().substring(1)))
   );
 };
 
